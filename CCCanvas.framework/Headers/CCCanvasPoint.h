2f928903f3c733bc128c28c0b125509ebef53dde
CC (deng you hua) <ccworld1000@gmail.com>
2021-08-23 10:33:19 +0800

    CCAutoTag : fix unit

--- CCCanvas.framework/Headers/CCCanvasPoint.h
+++ CCCanvas.framework/Headers/CCCanvasPoint.h
@@ -15,17 +15,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CCCanvasPoint : NSObject
 
-// the value [-1.0 - 1.0]
-@property (nonatomic, readonly) CGPoint pointA;
-@property (nonatomic, readonly) CGPoint pointB;
-@property (nonatomic, readonly) CGPoint pointC;
-@property (nonatomic, readonly) CGPoint pointD;
-
-@property (nonatomic, readonly) CGPoint pointE;
-@property (nonatomic, readonly) CGPoint pointF;
-@property (nonatomic, readonly) CGPoint pointG;
-@property (nonatomic, readonly) CGPoint pointH;
-
 @property (nonatomic, strong, nullable, readonly) UIImage *image;
 
 // canvas type [defalt type is CCCanvasTypeColor]
@@ -33,14 +22,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property (nonatomic, readonly) NSUInteger pointBytesSize;
 
+// CGPoint: the value [-1.0 - 1.0] for all init point
 
 -(instancetype)initPointWith1Point:(CGPoint) pointA ;
 -(instancetype)initLinetWith2Point:(CGPoint) pointA pointB: (CGPoint) pointB ;
 -(instancetype)initTriangleWith3Point:(CGPoint) pointA pointB: (CGPoint) pointB pointC: (CGPoint) pointC;
 -(instancetype)initPlaneWith4Point:(CGPoint) pointA  pointB: (CGPoint) pointB pointC: (CGPoint) pointC pointD: (CGPoint) pointD;
 -(instancetype)initTextureWith4PointOrImage:(CGPoint) pointA  pointB: (CGPoint) pointB pointC: (CGPoint) pointC pointD: (CGPoint) pointD image: (nullable UIImage *) image;
+
+
 -(instancetype)initPyamid;
 -(instancetype)initHexagon;
+-(instancetype)initHeptagonal;
 
 - (float *) pointBytes;
 
